994d4726cd2fc8fac506e30a1b9930d37e82894f
Maxime Charland <mcharland@google.com>
2020-07-28 16:22:03 -0400

    Allow for fragment to cross inline elements.

--- text-fragments.js
+++ text-fragments.js
@@ -18,6 +18,15 @@
     return;
   }
   const FRAGMENT_DIRECTIVES = ['text'];
+  // Block elements. elements of a text fragment cannot cross the boundaries of a block element.
+  // Source for the list : https://www.w3schools.com/html/html_blocks.asp
+  const BLOCK_ELEMENTS = [
+    "ADDRESS", "ARTICLE", "ASIDE", "BLOCKQUOTE", "CANVAS",
+    "DD", "DIV", "DL", "DT", "FIELDSET", "FIGCAPTION", "FIGURE",
+    "FOOTER", "FORM", "H1", "H2", "H3", "H4", "H5", "H6", "HEADER",
+    "HR", "LI", "MAIN", "NAV", "NOSCRIPT", "OL", "P", "PRE", "SECTION",
+    "TABLE", "TFOOT", "UL", "VIDEO"
+  ];
 
   Location.prototype.fragmentDirective = {};
 
@@ -88,81 +97,139 @@
     return s.replace(/[\\^$.*+?()[\]{}|]/g, '\\$&');
   };
 
-  const processTextFragmentDirective = (textFragment) => {
+  /**
+   * Highlights a text fragment and optionnally scrolls to it.
+   * @param textFragment - Fragment to highlight.
+   * @param {boolean} scrollTo - If true, the viewport will be scrolled to the text fragment.
+   */
+  const processTextFragmentDirective = (textFragment, scrollTo) => {
     const prefixNodes = findText(textFragment.prefix);
     const textStartNodes = findText(textFragment.textStart);
     const textEndNodes = findText(textFragment.textEnd);
     const suffixNodes = findText(textFragment.suffix);
     const scrollBehavior = {
-      behavior: 'smooth',
-      block: 'nearest',
+      behavior: 'auto',
+      block: 'center',
       inline: 'nearest',
     };
     if (
       !prefixNodes.length &&
       !suffixNodes.length &&
-      textStartNodes.length === 1 &&
-      textStartNodes[0].parentNode
+      textStartNodes.length === 1
     ) {
+      let startNode;
+      let startOffset;
+      let endNode;
+      let endOffset;
       // Only `textStart`
       if (!textEndNodes.length) {
-        const textStartNode = textStartNodes[0].parentNode;
-        const adjacentHTML = textStartNodes[0].textContent.replace(
-          new RegExp(
-            `(^.*?)(${escapeRegExp(textFragment.textStart)})(.*?$)`,
-            'm',
-          ),
-          '$1<mark>$2</mark>$3',
-        );
-        textStartNode.textContent = '';
-        textStartNode.insertAdjacentHTML('afterbegin', adjacentHTML);
-        textStartNode.scrollIntoView(scrollBehavior);
+        [startNode, startOffset] = findRangeNodeAndOffset(textStartNodes[0], textFragment.textStart, true);
+        [endNode, endOffset] = findRangeNodeAndOffset(textStartNodes[0], textFragment.textStart, false);
         // Only `textStart` and `textEnd`
-      } else if (textEndNodes.length === 1 && textEndNodes[0].parentNode) {
-        // If `textStart` and `textEnd` are in the same node
-        if (textEndNodes[0].parentNode === textStartNodes[0].parentNode) {
-          const textStartNode = textStartNodes[0].parentNode;
-          const adjacentHTML = textStartNodes[0].textContent.replace(
-            new RegExp(
-              `(^.*?)(${escapeRegExp(textFragment.textStart)})(.*?)(${
-                textFragment.textEnd
-              })(.*?$)`,
-              'm',
-            ),
-            '$1<mark>$2$3$4</mark>$5',
-          );
-          textStartNode.textContent = '';
-          textStartNode.insertAdjacentHTML('afterbegin', adjacentHTML);
-          textStartNode.scrollIntoView(scrollBehavior);
-          // If `textStart` and `textEnd` are in different nodes
-        } else {
-        }
+      } else if (textEndNodes.length === 1) {
+        [startNode, startOffset] = findRangeNodeAndOffset(textStartNodes[0], textFragment.textStart, true);
+        [endNode, endOffset] = findRangeNodeAndOffset(textEndNodes[0], textFragment.textEnd, false);
       }
+      let range = document.createRange();
+        range.setStart(startNode, startOffset);
+        range.setEnd(endNode, endOffset);
+        const mark = document.createElement("mark");
+        try {
+          range.surroundContents(mark);
+        } catch (ex) {
+          const commonAncestor = range.commonAncestorContainer;
+          while(startNode.parentNode !== commonAncestor) {
+            startNode = startNode.parentNode;
+            range.setStartBefore(startNode);
+          }
+          while(endNode.parentNode !== commonAncestor) {
+            endNode = endNode.parentNode;
+            range.setEndAfter(endNode);
+          }
+          range.surroundContents(mark);
+        }
+        window.setTimeout(() => {
+          if(scrollTo) {
+            mark.scrollIntoView(scrollBehavior);
+          }
+          let sel = window.getSelection();
+          let selRange = document.createRange();
+          selRange.selectNode(mark);
+          sel.addRange(selRange);
+        }, 1);
     }
     if (prefixNodes.length) {
     }
   };
 
+  /**
+   * Finds the DOM Node and the exact offset where a string starts or ends. 
+   * @param {Node} blockNode - Block node in which to search for a given text.
+   * @param {string} text - The text for which to find the position.
+   * @param {boolean} start - Whether to return the an offset for the start or the text, or the end.
+   * @returns {[Node, number]} The DOM Node and the offset where the text starts or ends.
+   */
+  const findRangeNodeAndOffset = (blockNode, text, start) => {
+    let offset = blockNode.textContent.indexOf(text) + (start ? 0 : text.length);
+    let startChildren = [];
+    const treeWalker = document.createTreeWalker(blockNode, NodeFilter.SHOW_TEXT);
+    let node = treeWalker.nextNode();
+    if(node) {
+      startChildren.push({
+        node,
+        start: 0,
+        end: node.textContent.length
+      });
+    }
+    while(!!(node = treeWalker.nextNode())) {
+      startChildren.push({
+        node: node,
+        start: startChildren[startChildren.length - 1].end,
+        end: startChildren[startChildren.length - 1].end + node.textContent.length
+      });
+    }
+    let anchorNode;
+    for(let {node, start, end} of startChildren) {
+      if(offset >= start && offset < end) {
+        anchorNode = node;
+        offset -= start;
+        break;
+      }
+    }
+    return [anchorNode, offset];
+  }
+
+  /**
+   * Find the deepest blocks node that contains the given text.
+   * @param {string} text - Text to search for.
+   * @returns {Node[]} - Nodes that fully contain the given text. None of the returned nodes contain block nodes.
+   */
   const findText = (text) => {
     if (!text) {
       return [];
     }
     const body = document.body;
-    const treeWalker = document.createTreeWalker(body, NodeFilter.SHOW_TEXT, {
+    const treeWalker = document.createTreeWalker(body, NodeFilter.SHOW_ELEMENT, {
       acceptNode: (node) => {
-        if (node.textContent.includes(text)) {
+        if(!BLOCK_ELEMENTS.includes(node.tagName)) {
+          return NodeFilter.FILTER_REJECT;
+        }
+        if([...node.childNodes].some(n => BLOCK_ELEMENTS.includes(n.tagName))) {
+          return NodeFilter.FILTER_SKIP;
+        }
+        if (node.textContent.includes(text)) { ///!!! Will not find nodes smaller than the searched-for text
           return NodeFilter.FILTER_ACCEPT;
         }
       },
     });
 
     const nodeList = [];
-    let currentNode = treeWalker.currentNode;
+    let currentNode = treeWalker.nextNode();
     while (currentNode) {
       nodeList.push(currentNode);
       currentNode = treeWalker.nextNode();
     }
-    return nodeList.slice(1);
+    return nodeList;
   };
 
   const init = () => {
@@ -177,5 +244,5 @@
     );
   };
 
-  init();
+  document.addEventListener("DOMContentLoaded", init, false);
 })();
