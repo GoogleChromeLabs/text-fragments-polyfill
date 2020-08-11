cbb43302bc93a7eac2335b6999b151d1cd6a078d
Tommy Martino <tmartino@google.com>
2020-08-11 18:34:02 -0400

    Add boundary point and string normalization utils

--- src/test/text-fragment-utils-test.js
+++ src/test/text-fragment-utils-test.js
@@ -32,7 +32,7 @@ describe('TextFragmentUtils', function () {
     );
   });
 
-  it('works with complexe layouts', function () {
+  it('works with complex layouts', function () {
     document.body.innerHTML = window.__html__['complicated-layout.html'];
     const directives = utils.getFragmentDirectives(
       '#:~:text=is%20a%20test,And%20another%20on',
@@ -77,5 +77,69 @@ describe('TextFragmentUtils', function () {
     range.setEnd(document.getElementById('e').nextSibling, 6);
     const marks = utils.forTesting.markRange(range);
     expect(marksArrayToString(marks)).toEqual('elaborate fancy div');
+  });
+
+  it('can normalize text', function () {
+    // Dict mapping inputs to expected outputs.
+    const testCases = {
+      '': '',
+      ' foo123 ': ' foo123 ',
+      // Various whitespace is collapsed
+      '\n Kirby\t Puckett   ': ' Kirby Puckett ',
+      // Latin accent characters are removed
+      ñîçè: 'nice',
+      // Some Japanese characters like パ can be rendered with 1 or 2 code
+      // points; the normalized version will always use two.
+      '『パープル・レイン』': '『ハ\u309Aーフ\u309Aル・レイン』',
+      // Chinese and Russian don't use diacritics and are unchanged.
+      紫雨: '紫雨',
+      'Кирилл Капризов': 'Кирилл Капризов',
+    };
+
+    for (const input of Object.getOwnPropertyNames(testCases)) {
+      expect(utils.forTesting.normalizeString(input)).toEqual(testCases[input]);
+    }
+  });
+
+  it('can find boundary points', function () {
+    document.body.innerHTML = __html__['boundary-points.html'];
+    const rootNode = document.getElementById('root');
+    const walker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT);
+    const textContent = utils.forTesting.normalizeString(rootNode.textContent);
+    const allTextNodes = [];
+    let textNode = walker.nextNode();
+    while (textNode != null) {
+      allTextNodes.push(textNode);
+      textNode = walker.nextNode();
+    }
+
+    // Test cases are substrings of the normalized text content of the document.
+    // The goal in each case is to produce a range pointing to the substring.
+    const testCases = [
+      ' this text has a',
+      ' has a lot of ハ\u309A,',
+      ' フ\u309A stuff gøing on ',
+    ];
+
+    for (const input of testCases) {
+      const startIndex = textContent.search(input);
+      expect(startIndex).not.toEqual(-1);
+      const endIndex = startIndex + input.length;
+
+      const start = utils.forTesting.getBoundaryPointAtIndex(
+        startIndex,
+        allTextNodes,
+        /* isEnd= */ false,
+      );
+      const end = utils.forTesting.getBoundaryPointAtIndex(
+        endIndex,
+        allTextNodes,
+        /* isEnd= */ true,
+      );
+      const range = document.createRange();
+      range.setStart(start[0], start[1]);
+      range.setEnd(end[0], end[1]);
+      expect(utils.forTesting.normalizeString(range.toString())).toEqual(input);
+    }
   });
 });
