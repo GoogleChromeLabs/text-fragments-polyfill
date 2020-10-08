c4674acf3c7278b58a92dd65b194fa9cb2594194
Tommy Martino <tmartino@google.com>
2020-10-08 12:12:33 -0400

    Disabling sandbox in test

--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,13 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+          base: 'ChromeHeadless',
+          flags: ['--no-sandbox']
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
