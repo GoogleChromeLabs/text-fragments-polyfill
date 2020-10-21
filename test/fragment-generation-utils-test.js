5104ede8424218e9732252449b889869687a4964
Tommy Martino <31107939+tfmar@users.noreply.github.com>
2020-10-21 17:18:20 -0400

    Respecting end of word/block boundaries (#33)

--- test/fragment-generation-utils-test.js
+++ test/fragment-generation-utils-test.js
@@ -20,7 +20,7 @@ describe('FragmentGenerationUtils', function() {
     expect(result.fragment.suffix).toBeUndefined();
   });
 
-  it('can find a word boundary inside a text node', function() {
+  it('can find a word start inside a text node', function() {
     document.body.innerHTML = __html__['word_bounds_test.html'];
     const elt = document.getElementById('block');
 
@@ -46,6 +46,32 @@ describe('FragmentGenerationUtils', function() {
     expect(result).toEqual(-1);
   });
 
+  it('can find a word end inside a text node', function() {
+    document.body.innerHTML = __html__['word_bounds_test.html'];
+    const elt = document.getElementById('block');
+
+    // elt is an HTML element, not a text node, so we should find -1
+    let result = utils.forTesting.findWordEndBoundInTextNode(elt);
+    expect(result).toEqual(-1);
+
+    const node = elt.firstChild;
+    // With no second arg, we find the first
+    result = utils.forTesting.findWordEndBoundInTextNode(node);
+    expect(result).toEqual(6);  // Between "e" and " "
+
+    // Second arg in the middle of a word
+    result = utils.forTesting.findWordEndBoundInTextNode(node, 2);
+    expect(result).toEqual(6);  // Between "e" and " "
+
+    // Second arg immediately *after* a space should give the same output
+    result = utils.forTesting.findWordEndBoundInTextNode(node, 7);
+    expect(result).toEqual(7)
+
+    // No more spaces to the right of second arg, -1
+    result = utils.forTesting.findWordEndBoundInTextNode(node, 10);
+    expect(result).toEqual(-1);
+  });
+
   it('can expand a range start to a word bound within a node', function() {
     document.body.innerHTML = __html__['word_bounds_test.html'];
     const range = document.createRange();
@@ -57,6 +83,19 @@ describe('FragmentGenerationUtils', function() {
 
     utils.forTesting.expandRangeStartToWordBound(range);
     expect(range.toString()).toEqual('block');
+  });
+
+  it('can expand a range end to a word bound within a node', function() {
+    document.body.innerHTML = __html__['word_bounds_test.html'];
+    const range = document.createRange();
+    const textNodeInBlock = document.getElementById('block').firstChild;
+
+    range.setStart(textNodeInBlock, 0);
+    range.setEnd(textNodeInBlock, 3);
+    expect(range.toString()).toEqual('Ins');
+
+    utils.forTesting.expandRangeEndToWordBound(range);
+    expect(range.toString()).toEqual('Inside');
   });
 
   it('can expand a range start to an inner block boundary', function() {
@@ -72,24 +111,52 @@ describe('FragmentGenerationUtils', function() {
     expect(range.toString()).toEqual('Inside block');
   });
 
-  it('can expand a range start across inline elements', function() {
+  it('can expand a range end to an inner block boundary', function() {
+    document.body.innerHTML = __html__['word_bounds_test.html'];
+    const range = document.createRange();
+    const textNodeInBlock = document.getElementById('block').firstChild;
+
+    range.setStart(textNodeInBlock, 0);
+    range.setEnd(textNodeInBlock, 10);
+    expect(range.toString()).toEqual('Inside blo');
+
+    utils.forTesting.expandRangeEndToWordBound(range);
+    expect(range.toString()).toEqual('Inside block');
+  });
+
+  it('can expand a range end across inline elements', function() {
     document.body.innerHTML = __html__['word_bounds_test.html'];
     const range = document.createRange();
     const inlineTextNode = document.getElementById('inline').firstChild;
-    const lastTextNode = document.getElementById('root').lastChild;
+    // Get the text node between the <p> and <i> nodes:
+    const middleTextNode = document.getElementById('root').childNodes[2];
 
-    range.setStart(inlineTextNode, 1);
-    range.setEnd(lastTextNode, 2);
-    expect(range.toString()).toEqual('ine');
+    range.setStart(middleTextNode, 3);
+    range.setEnd(inlineTextNode, 2);
+    expect(range.toString()).toEqual('Inli');
 
-    utils.forTesting.expandRangeStartToWordBound(range);
+    utils.forTesting.expandRangeEndToWordBound(range);
     expect(range.toString()).toEqual('Inline');
 
-    range.setStart(lastTextNode, 1);
-    range.setEnd(lastTextNode, 2);
-    expect(range.toString()).toEqual('e');
+    range.setStart(middleTextNode, 3);
+    range.setEnd(middleTextNode, 5);
+    expect(range.toString()).toEqual('In');
 
-    utils.forTesting.expandRangeStartToWordBound(range);
+    utils.forTesting.expandRangeEndToWordBound(range);
+    console.log(range.endContainer.nodeType + range.endContainer.tagName);
     expect(range.toString()).toEqual('Inline');
   });
+
+  it('can postorder traverse', function() {
+    document.body.innerHTML = __html__['postorder-tree.html'];
+    const walker = document.createTreeWalker(document.getElementById('h'));
+    walker.currentNode = document.getElementById('b').firstChild;
+    const visited = utils.forTesting.prepareVisitedSet(walker);
+    const traversalOrder = [];
+    while (utils.forTesting.forwardTraverse(walker, visited) != null) {
+      if (walker.currentNode.id != null)
+        traversalOrder.push(walker.currentNode.id);
+    }
+    expect(traversalOrder).toEqual(['b', 'c', 'e', 'g', 'f', 'd', 'h']);
+  })
 });
